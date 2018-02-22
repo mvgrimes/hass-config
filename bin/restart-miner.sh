3e2bc28dd747adfe17fd52301bd0b324ac67c808
Mark Grimes <mgrimes@cpan.org>
2018-02-22 08:07:35 -0500

    Re-adds the password flag to restart-miner.sh
    Antminer won't save diddly b/w reboots. This includes passwd and
    .ssh/authorized_keys. So we need to use `sshpass` and the default
    username/password. Well at least it is behind a firewall.

--- bin/restart-miner.sh
+++ bin/restart-miner.sh
@@ -2,19 +2,21 @@
 
 miner="antminer"
 user="root"
+password="admin"
 
 usage() {
-  echo "usage: restart-miner -m <miner IP or host> -u <user>"
+  echo "usage: restart-miner -m <miner IP or host> -u <user> -p <password>"
   echo $@
   echo "   -u  miner ssh username"
   echo "   -m  miner address"
+  echo "   -p  miner password"
   echo "   -h  help"
   exit 1
 }
 
 while getopts ":p:u:m:h" opt; do
     case $opt in
-        p)  usage "Use ssh keys"                          ;;
+        p)  password="$OPTARG"                            ;;
         u)  user="$OPTARG"                                ;;
         m)  miner="$OPTARG"                               ;;
         \?) usage "Invalid option: -$OPTARG"              ;;
@@ -25,4 +27,4 @@ done
 # Decrements the argument pointer so it points to the next argument
 shift $(($OPTIND - 1))
 
-ssh "$user@$miner" '/sbin/reboot'
+sshpass -p "$password" ssh "$user@$miner" '/sbin/reboot'
