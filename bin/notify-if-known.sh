501256a07b3fb8201b101671ae7945703a6fcfeb
Mark Grimes <mgrimes@cpan.org>
2018-05-28 12:37:16 -0400

    Uses the TTS get_url API to create .mp3 for mqtt-say

--- bin/notify-if-known.sh
+++ bin/notify-if-known.sh
@@ -3,32 +3,27 @@
 url="http://localhost:8123"
 topic_ring="hass/mqtt-say/ring"
 topic_say="hass/mqtt-say/message"
-# database="/Users/mgrimes/src/hass/config/phones.sqlite"
-# pysqlite="/Users/mgrimes/src/hass/config/bin/check-is-known.py"
-# logfile="/Users/mgrimes/src/hass/config/nik.log"
-database="/srv/app/hass/config/phones.sqlite"
-pysqlite="/srv/app/hass/config/bin/check-is-known.py"
-logfile="/srv/app/hass/config/nik.log"
-
-[ -t 0 ] && exec >> "$logfile" 2>&1
+config_dir="/config"
 
 usage() {
-  echo "usage: notify-if-known -p <phone number> -n <name> -H <http API password> -t <mqtt topic>"
+  echo "usage: notify-if-known -p <phone number> -n <name> -d <config dir> -H <http API password> -t <mqtt topic>"
   echo $@
   echo "   -p  phone number to lookup"
   echo "   -n  name to use in notification"
   echo "   -t  topic to publish to (default: $topic)"
   echo "   -H  <http API password>"
+  echo "   -d  config dir (default: $config_dir)"
   echo "   -h  help"
   exit 1
 }
 
-while getopts ":p:n:t:H:" opt; do
+while getopts ":p:n:t:H:d:" opt; do
     case $opt in
         p)  phone="$OPTARG"                               ;;
         n)  name="$OPTARG"                                ;;
         t)  topic="$OPTARG"                               ;;
         H)  http_api_password="$OPTARG"                   ;;
+        d)  config_dir="$OPTARG"                   ;;
         \?) usage "Invalid option: -$OPTARG"              ;;
         :)  usage "Option -$OPTARG requires an argument." ;;
     esac
@@ -37,17 +32,35 @@ done
 # Decrements the argument pointer so it points to the next argument
 shift $(($OPTIND - 1))
 
+database="$config_dir/phones.sqlite"
+pysqlite="$config_dir/bin/check-is-known.py"
+logfile="$config_dir/nik.log"
+
+# [ -t 0 ] && exec >> "$logfile" 2>&1
+
 # TODO: ensure payload is properly escaped
 mqtt-publish() {
     local topic=$1
     local payload=$2
 
-    echo "publish: $payload"
+    echo "publish: $payload" >&2
     curl -s -X POST \
         -H "x-ha-access: $http_api_password" \
         -H "Content-TYpe: application/json" \
         -d "{\"topic\": \"$topic\", \"payload\": \"$payload\"}" \
         "$url/api/services/mqtt/publish"
+
+}
+
+get-tts-file() {
+    local text="$1"
+
+    echo "get-tts-file: $text" >&2
+    curl -s -X POST \
+        -H "x-ha-access: $http_api_password" \
+        -H "Content-TYpe: application/json" \
+        -d "{\"message\": \"$text\", \"platform\": \"google\"}" \
+        "$url/api/tts_get_url" | jq -r .url
 }
 
 is-friend() {
@@ -55,7 +68,7 @@ is-friend() {
     phone=$( echo "$1" | perl -pE's{\D}{}g; s{^1}{}g' )
     [ -n "$phone" ] || return 1  # false
 
-    echo "checking $phone"
+    echo "checking $phone" >&2
     res=$( $pysqlite "$database" "$phone" )
     err=$?
 
@@ -63,7 +76,8 @@ is-friend() {
     # echo "is-friend => $err"
 
     if [ -n "$res" ] ; then
-        [ -z "$name" ] && name="$res"
+        # [ -z "$name" ] && name="$res"
+        name="$res"
         true
     else
         false
@@ -75,8 +89,10 @@ is-friend() {
 
 if is-friend "$phone" ; then
     [ -n "$name" ] || name="$phone"
-    mqtt-publish "$topic_ring" "short-ring"
-    # mqtt-publish "$topic_say" "Incoming call from $name"
+    # mqtt-publish "$topic_ring" "short-ring"
+
+    tts_url=$( get-tts-file "Incoming call from $name" )
+    mqtt-publish "$topic_say" "$tts_url"
 else
-    echo "We don't know you!"
+    echo "We don't know you!" >&2
 fi
