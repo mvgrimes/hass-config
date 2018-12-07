b9dfc63b6dd6d1b3ed5f423bc393294c4a47f296
Mark Grimes <mgrimes@cpan.org>
2018-12-06 19:05:57 -0500

    Scrape more data directly from the miner
    If we want to use a different pool we can't rely on the pool software

--- bin/scrape-miner.sh
+++ bin/scrape-miner.sh
@@ -2,17 +2,54 @@
 
 post(){
   echo "posting $@"
-  curl -is -XPOST 'http://192.168.1.201:8086/write?db=home_assistant' --data-binary "$@"
+  curl -s -XPOST 'http://192.168.1.201:8086/write?db=home_assistant' --data-binary "$@"
+}
+
+get-summary(){
+  echo -n '{"command":"summary"}' | fetch
+}
+
+get-stats(){
+  echo -n '{"command":"stats"}' | fetch | perl -pe's!}\{!},\{!'
 }
 
 fetch(){
-  echo -n '{"command":"summary"}' | nc antminer 4028
+  nc antminer 4028
 }
 
-rate=$( fetch | jq '.SUMMARY[0]."GHS 5s"' )
-post "miner_rate,miner=antminer_1 value=$rate"
+default() {
+  rate=$( get-summary | jq '.SUMMARY[0]."GHS 5s"' )
+  post "miner_rate,miner=antminer_1 value=$rate"
+
+  stats=$( get-stats )
+
+  temp1=$( echo $stats | jq '.STATS[1]."temp1"' )
+  post "miner_temp,asc=1,miner=antminer_1 value=$temp1"
+
+  temp2=$( echo $stats | jq '.STATS[1]."temp2"' )
+  post "miner_temp,asc=2,miner=antminer_1 value=$temp2"
+
+  temp3=$( echo $stats | jq '.STATS[1]."temp3"' )
+  post "miner_temp,asc=3,miner=antminer_1 value=$temp3"
+
+  fan1=$( echo $stats | jq '.STATS[1]."fan1"' )
+  post "miner_fan,fan=1,miner=antminer_1 value=$fan1"
+
+  fan3=$( echo $stats | jq '.STATS[1]."fan3"' )
+  post "miner_fan,fan=3,miner=antminer_3 value=$fan3"
+}
+
+main(){
+  if [ -z "$@" ] ; then
+    default
+  else
+    $@
+  fi
+}
 
 # run_or_die( $curl . "'miner_shares,miner=antminer_1 value=$shares'");
 # run_or_die( $curl . "'miner_payout,miner=antminer_1 value=$payout'");
 # run_or_die( $curl . "'miner_rate,miner=antminer_1 value=$rate'");   4.665k
 # run_or_die( $curl . "'miner_t2s,miner=antminer_1 value=$t2s'");
+
+main $@
